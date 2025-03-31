cce87f78b56b40c20292f18aa649df55802dc995
Jaokimben <joakimben1234@gmail.com>
2025-03-31 12:07:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,10 +45,9 @@ def scrap_seloger():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-    chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN')
     
-    service = Service(executable_path=os.getenv('CHROMEDRIVER_PATH'))
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    # Utilisation du ChromeDriver par défaut
+    driver = webdriver.Chrome(options=chrome_options)
     
     try:
         villes = ['Paris', 'Lyon', 'Marseille', 'Bordeaux', 'Toulouse', 'Nantes', 'Lille']
