213e8269052ee807f765d2b7dbfdb4aeec88c388
Jaokimben <joakimben1234@gmail.com>
2025-03-31 12:46:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from sqlalchemy import or_
 import random
 import requests
 import re
+from webdriver_manager.chrome import ChromeDriverManager
 
 load_dotenv()
 
@@ -47,9 +48,8 @@ def scrap_seloger():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-    chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN')
     
-    service = Service(executable_path=os.getenv('CHROMEDRIVER_PATH'))
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     
     try:
